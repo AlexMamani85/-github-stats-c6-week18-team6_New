888286da1ab8a72df7c1ed34d1caa67ab95500e8
Revocod3 <kevinlopez2693@gmail.com>
2022-06-24 00:55:53 -0400

    Add: Un&AuthenticatedApp

---
+++ src/AuthenticatedApp.js
@@ -0,0 +1,9 @@
+const AuthenticatedApp = () => {
+  return (
+    <div>
+      <h1>Authenticated App</h1>
+    </div>
+  );
+};
+
+export default AuthenticatedApp;
---
+++ src/UnauthenticatedApp.js
@@ -0,0 +1,9 @@
+const UnauthenticatedApp = () => {
+  return (
+    <div>
+      <h1>Unauthenticated App</h1>
+    </div>
+  );
+};
+
+export default UnauthenticatedApp;
